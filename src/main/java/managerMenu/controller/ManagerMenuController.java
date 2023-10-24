61e16a4b86abd51c58d2a6f4f7b0ee802d99c4fa
GithubWoon <eo2458@naver.com>
2023-10-24 17:50:45 +0900

    아씨발

--- src/main/java/managerMenu/controller/ManagerMenuController.java
+++ src/main/java/managerMenu/controller/ManagerMenuController.java
@@ -9,15 +9,18 @@ import managerMenu.service.ManagerMenuService;
 
 @Controller
 public class ManagerMenuController {
-	private final ManagerMenuService managerMenuService;
+
+    private final ManagerMenuService managerMenuService;
 
     public ManagerMenuController(ManagerMenuService managerMenuService) {
         this.managerMenuService = managerMenuService;
     }
-    @PostMapping("/managerMenu/ManagerMenuAction") // 사용자가 지정한 URL 패턴으로 변경해주세요.
+
+    @PostMapping("/managerMenu/ManagerMenuAction")
     public ModelAndView managerMenuAction(@RequestParam String action) {
-        managerMenuService.processManagerAction(action);
-        // 처리 결과에 따른 ModelAndView 반환
-        return new ModelAndView("managerMenu"); // 반환할 뷰 이름을 지정해주세요.
+
+        ModelAndView modelAndView = managerMenuService.processManagerAction(action);
+
+        return modelAndView;
     }
 }
--- src/main/java/managerMenu/service/ManagerMenuService.java
+++ src/main/java/managerMenu/service/ManagerMenuService.java
@@ -9,6 +9,7 @@ public class ManagerMenuService {
         if ("메뉴판 보기".equals(action)) {
             // 메뉴판 보기에 대한 로직 처리
         } else if ("재고수량 추가".equals(action)) {
+            modelAndView.setViewName("test");
             // 재고수량 추가에 대한 로직 처리
         } else if ("구매내역 보기".equals(action)) {
             // 구매내역 보기에 대한 로직 처리
